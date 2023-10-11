ac02ab7be8e8ca456eb6dab0242349a214296368
Kellyson the creator <kellysonbtm@unipam.edu.br>
2023-10-11 00:15:16 -0300

    PI-Dados-Prontuarios

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@
 import './App.css';
 import './components/header/Header';
 import Header from './components/header/Header';
+import DadosProntuarios from './components/header/dados-prontuarios/dados-prontuarios';
+
 
 
 function App() {
   return (
   <div>
+
+      <div className='header'>
+        <Header />
+      </div>
+
+      <div className='prontuarios'>
+        <DadosProntuarios />
+      </div>
+
   </div>
   );
 }
--- src/components/header/dados-prontuarios/dados-prontuarios.jsx
+++ src/components/header/dados-prontuarios/dados-prontuarios.jsx
@@ -1,12 +1,30 @@
-import './dados-prontuarios.css';
-import Header from'../Header'
+import './dados-prontuarios.css'
+import folder from '../../../assets/folder.png'
 
-function dadosprontuarios() {
+const MinhasPastas = () => {
   
+    const pastas = [
+        'Amanda',
+        'Bianca',
+        'Charles'
+    ].sort();
+
     return (
-        <div>
-            <Header />
+    <div>
+        <div className="p-mid">
+            <h1>Minhas Pastas</h1>
+            <div className="b-folder">
+                <div className="p-folder">
+                    <img src={folder} alt="Ícone de Pasta" />
+                    <p>Pasta 1</p>
+                </div>
+                <div className="p-folder">
+                    <img src={folder} alt="Ícone de Pasta" />
+                    <p>Pasta 2 </p>
+                </div>
+            </div>
         </div>
+    </div>
 )
 }
-export default dadosprontuarios;
+export default MinhasPastas;
