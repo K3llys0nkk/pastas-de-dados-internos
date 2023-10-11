d6b45059774fce8cc4795bb2759d41735ed6e47b
Kellyson the creator <kellysonbtm@unipam.edu.br>
2023-10-11 02:27:12 -0300

    correcao-tela

--- src/components/header/dados-prontuarios/dados-prontuarios.jsx
+++ src/components/header/dados-prontuarios/dados-prontuarios.jsx
@@ -111,6 +111,38 @@ const MinhasPastas = () => {
                     </div>
                 </div>
             </div>
+            <div className="folder-right">
+                <div className="b-folder">
+                    <div className="p-folder">
+                        <img src={folder} alt="Ícone de Pasta" />
+                        <p>Nome: </p>
+                        <p>Igor Castro</p> 
+                        <p className='p-cpf'>CPF:</p>
+                        <p>XXX.XXX.XXX-XX</p> 
+                    </div>
+                    <div className="p-folder">
+                        <img src={folder} alt="Ícone de Pasta" />
+                        <p>Nome: </p>
+                        <p>João Gomes</p> 
+                        <p className='p-cpf'>CPF:</p>
+                        <p>XXX.XXX.XXX-XX</p> 
+                    </div>
+                    <div className="p-folder">
+                        <img src={folder} alt="Ícone de Pasta" />
+                        <p>Nome: </p>
+                        <p>Kellyson B</p> 
+                        <p className='p-cpf'>CPF:</p>
+                        <p>XXX.XXX.XXX-XX</p> 
+                    </div>
+                    <div className="p-folder">
+                        <img src={folder} alt="Ícone de Pasta" />
+                        <p>Nome: </p>
+                        <p>Luccas Neto</p> 
+                        <p className='p-cpf'>CPF:</p>
+                        <p>XXX.XXX.XXX-XX</p> 
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 )
